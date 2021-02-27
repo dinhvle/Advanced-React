14a67aab76ebb1645c0ce37f5bc5cc37f923a24f
Dinh Le <dinh.v.le@gmail.com>
2021-02-26 22:16:57 -0600

    create ProductImage Data Type

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -42,6 +43,7 @@ export default withAuth(
       // Schema items go here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // Show UI only for authorize people
