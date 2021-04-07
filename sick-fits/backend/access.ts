fe5282194f72735c93fb41c57c843c8a07d5a19a
Dinh Le <dinh.v.le@gmail.com>
2021-04-06 22:46:19 -0500

    permissions access function

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,6 +1,22 @@
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 // The access control return true or false value depending on the users' session.
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+export const permissions = {
+  canManageProducts({ session }) {
+    return session?.data.role?.canManageProducts;
+  },
+};
